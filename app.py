6fa0db986e003b0f5786e81b44fea9a427e37f9b
Sahana Roy <102234189+sahana-roy@users.noreply.github.com>
2023-12-22 15:23:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,5 @@ def get_suggestions():
         'companies': list(co_occurring_companies)
     })
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
